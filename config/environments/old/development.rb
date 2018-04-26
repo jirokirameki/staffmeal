55c0a294306f38d3abb91893cafed8fdd49a0fdc
jirokirameki <jirokirameki@yahoo.co.jp>
2018-04-26 00:41:52 +0000

    comment out send mail

--- config/environments/old/development.rb
+++ config/environments/old/development.rb
@@ -27,20 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_url_options = { protocol: 'https' , host: 'b2819436efa0461ab93722d8646c9095.vfs.cloud9.us-east-2.amazonaws.com'}
-  # config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { protocol: 'https' , host: 'b2819436efa0461ab93722d8646c9095.vfs.cloud9.us-east-2.amazonaws.com'}
+  config.action_mailer.delivery_method = :sendmail
   
-  # config.action_mailer.smtp_settings = {
-  #   :enable_starttls_auto => true,
-  #   :address => "smtp.gmail.com",
-  #   :port => 587,
-  #   :domain => 'smtp.gmail.com',
-  #   :user_name => ENV['GOOGLE_ACCOUNT'], #gmailアドレス
-  #   :password => ENV['GOOGLE_APPLICATION_PASSWORD'], #gmailパスワード（アプリパスワード）
-  #   :authentication => 'login',
-  # }
-  # config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['GOOGLE_ACCOUNT'], #gmailアドレス
+    :password => ENV['GOOGLE_APPLICATION_PASSWORD'], #gmailパスワード（アプリパスワード）
+    :authentication => 'login',
+  }
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
